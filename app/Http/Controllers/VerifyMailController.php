6af4706fb82f46333a5b96300598f3a1c6a403ca
sohelahr <sohelahr@gmail.com>
2021-11-06 13:47:01 +0530

    Email verification issue solved

--- app/Http/Controllers/VerifyMailController.php
+++ app/Http/Controllers/VerifyMailController.php
@@ -7,17 +7,27 @@ use Illuminate\Http\Request;
 class VerifyMailController extends Controller
 {
     //
-    function Verify($id){
+    function Verify($id)
+    {
         $decoded_id = base64_decode($id);
         $user = User::find($decoded_id);
-        if($user->is_verified){
-            return redirect()->route('login')->with('already_verfied','123');
+        if(isset($user))
+        {
+            if($user->is_verified)
+            {
+                return redirect()->route('login')->with('already_verfied','123');
+            }
+            else
+            {
+                $user->is_verified = 1;
+                $user->email_verified_at = now();
+                $user->save();
+                return redirect()->route('login')->with('email_verfied','123');
+            }
         }
-        else{
-            $user->is_verified = 1;
-            $user->email_verified_at = now();
-            $user->save();
-            return redirect()->route('login')->with('email_verfied','123');
+        else
+        {
+            return redirect()->route('login')->with('error','Opps, Something went wrong');
         }
     }
 }
